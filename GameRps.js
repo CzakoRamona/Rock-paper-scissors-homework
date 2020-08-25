243912588389764c27fedbc996913cfb260f8954
CzakoRamona <drvizitiuramona@gmail.com>
2020-08-25 16:13:10 +0300

    Update GameRps.js
    varianta simpla 1

--- GameRps.js
+++ GameRps.js
@@ -83,3 +83,23 @@ function checkWinner(player, computer){
         }
     }
 }
+/* varianta simpla cu array 
+var rPs = ["Rock", "Paper", "Scissors"];
+// We need two random variables, one for 'you'
+var you = rPs[Math.floor(Math.random() * 3)];
+// and one for the 'computer'
+var computer = rPs[Math.floor(Math.random() * 3)];
+
+// Now invoke function
+random(you, computer);
+
+function random(you, computer){
+    if (you===computer){
+         console.log ("It's a tie!");
+    } else if (you==="Rock" && computer === "Scissors" || you === "Paper" && computer==="Rock"||you==="Scissors"&&computer==="Paper") {
+         console.log ("You've won!");
+    } else {
+         console.log ("You've lost!");
+    }
+}
+*/
